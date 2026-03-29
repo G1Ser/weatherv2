9a70d2c252c4815bf391dd552152366f00e96f37
Chauncey <chauncey1129@163.com>
2026-03-29 19:56:08 +0800

    fix: 修改CI/CD自动化部署

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+/**
+ * 环境配置模板文件
+ * @example 开发环境：复制为 environment.dev.ts
+ * @example 生产环境：复制为 environment.prod.ts
+ */
+export const environment = {
+  production: true,
+  ANGULAR_APP_BMAP_API: 'https://wt-prod-k8m3.chauncey.work',
+};
